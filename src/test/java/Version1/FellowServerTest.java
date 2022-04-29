b99434c08417a5fb7d1790f2c7dea690ade00525
wangchen <1244845520@qq.com>
2022-04-29 16:03:05 +0800

    fix test file

--- src/test/java/Version1/FellowServerTest.java
+++ src/test/java/Version1/FellowServerTest.java
@@ -8,20 +8,9 @@ import java.net.ServerSocket;
 
 public class FellowServerTest {
     public static void main(String[] args) throws IOException, InterruptedException {
-//        ServerSocket serverSocket = new ServerSocket(4321);
-//        PartitionController partition = new PartitionController("Test",0,false,1,1,serverSocket,null);
-//        PartitionAcceptSocketConnect partitionAcceptSocketConnect = new PartitionAcceptSocketConnect(partition);
-//        partitionAcceptSocketConnect.startServer();
-        System.out.println("Server running...");
-        System.out.println("accept a new connection");
-        System.out.println("accept a new connection");
-        System.out.println("accept a push request");
-        System.out.println("accept a pull request");
-        System.out.println("accept a pull request");
-        System.out.println("accept a pull request");
-        System.out.println("accept a pull request");
-        while (true){
-            Thread.sleep(1000);
-        }
+        ServerSocket serverSocket = new ServerSocket(4321);
+        PartitionController partition = new PartitionController("Test",0,false,1,1,serverSocket,null);
+        PartitionAcceptSocketConnect partitionAcceptSocketConnect = new PartitionAcceptSocketConnect(partition);
+        partitionAcceptSocketConnect.startServer();
     }
 }
